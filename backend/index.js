cafbc81e8adf67b5b7c49ee395ab0747bfe39bb2
Nitin kadiyan <kadiyanjatin99@gmail.com>
2025-11-25 13:39:46 +0530

    cors added

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin : "https://equityflow-stock-frontend.onrender.com",
+  origin :["https://equityflow-stock-frontend.onrender.com","https://equityflow-stock-trading-platform-svt3.onrender.com"] ,
   credentials:true,
 }));
 
