f1e9e377d92b1841afc1d7401bebd5fd120dc307
Nitin kadiyan <kadiyanjatin99@gmail.com>
2025-11-25 13:22:32 +0530

    home page

--- dashboard/src/components/Home.js
+++ dashboard/src/components/Home.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import Dashboard from './Dashboard.js';
-import TopBar from './TopBar.js';
+import React, { useEffect } from "react";
+import axios from "axios";
+
+import Dashboard from "./Dashboard.js";
+import TopBar from "./TopBar.js";
 
 function Home() {
-    useEffect(() => {
-  axios
-    .get("https://equityflow-stock-backend.onrender.com/api", {
-      withCredentials: true,
-    })
-    .then((res) => {
-      console.log("User authenticated:", res.data);
-    })
-    .catch((err) => {
-      console.log("Not authenticated, redirecting...");
-      window.location.href = "https://equityflow-stock-frontend.onrender.com/login";
-    });
-}, []);
-    return ( 
 
-        <>
-        <TopBar/>
-        <Dashboard/>
-        </>
-     );
+  useEffect(() => {
+    axios
+      .get("https://equityflow-stock-backend.onrender.com/api/auth/me", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        console.log("User authenticated:", res.data);
+      })
+      .catch((err) => {
+        console.log("Not authenticated, redirecting...");
+        window.location.href =
+          "https://equityflow-stock-frontend.onrender.com/login";
+      });
+  }, []);
+
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
 }
 
 export default Home;
